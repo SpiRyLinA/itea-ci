c0322bd7230cbe1874ca931beac4cf18688c3ed6
pozyvayl <dmitriy.pozyvaylo@gmail.com>
2019-10-05 09:48:35 +0300

    feat(package.json): trying to fix heroku error

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/itea-ci'));
-app.get('/*', function (req, resp) {
-  resp.sendFile(path.join(__dirname + '/dist/itea-ci/index.html'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/itea-ci/index.html'));
 });
-app.listen(process.env.PROD || 8080);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
